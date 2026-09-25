f2ec03103f587f5e64a7e85eb85201a49bacd23c
rhit-letojj <letojj@rose-hulman.edu>
2026-09-25 00:08:31 -0400

    Prototype Final

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 // Citation:
 // The solution for not duplicating html for the ribbon and footer across pages
-// was made with help from the solutions on _
+// was made with help from the solutions on a StackOverflow.com forum
+// https://stackoverflow.com/questions/38837835/include-html-in-another-html-file
+
+function fetchHtml() {
+  fetch('./ribbon.html')
+  .then((response) => {
+    return response.text();
+  })
+  .then((html) => {
+    document.getElementById("ribbon").innerHTML = html     
+  });
+
+  fetch('./footer.html')
+  .then((response) => {
+    return response.text();
+  })
+  .then((html) => {
+    document.getElementById("footer").innerHTML = html     
+  });
+}
+
+fetchHtml()
